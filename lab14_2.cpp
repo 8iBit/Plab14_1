e356cf6acbe821444ed89bea79371061e200c9a2
porrapat <iporrapat@gmail.com>
2023-01-13 17:40:01 +0700

    lab14_2.cpp

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,42 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool img[N][M],int s,int x,int y)
+{
+    double A,B,C;
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<M;j++)
+        {
+            A=pow(i-x,2);
+            B=pow(j-y,2);
+            C=sqrt(A+B);
+            if(C<=s-1)
+            {
+                img[i][j]=1;
+            }
+        }
+    }
+}
+
+void showImage(const bool img[N][M])
+{
+    cout << "------------------------------------------------------------------------\n";
+    for(int i=0;i<N;i++)
+    {
+        cout << "|";
+        for(int j=0;j<M;j++)
+        {
+            if (img[i][j]==1)
+                cout << "*";
+            else 
+            {
+                cout << " ";
+            }
+        }
+        cout << "|";
+        cout << "\n";
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
